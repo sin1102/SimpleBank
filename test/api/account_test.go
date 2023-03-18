294a84adae8201f3a54f280b5cb0193eff3e18d1
sin1102 <titanic1102341@gmail.com>
2023-03-18 17:52:39 +0700

    feat: add test api

---
+++ test/api/account_test.go
@@ -0,0 +1,49 @@
+package api
+
+import (
+	"fmt"
+	"learn/banking/api"
+	mockdb "learn/banking/db/mock"
+	db "learn/banking/db/sqlc"
+	"learn/banking/utils"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetAccountAPI(t *testing.T) {
+	account := randomAccount()
+
+	ctrl := gomock.NewController(t)
+	ctrl.Finish()
+
+	store := mockdb.NewMockStore(ctrl)
+
+	store.EXPECT().
+		GetAccount(gomock.Any(), gomock.Eq(account.ID)).
+		Times(1).
+		Return(account, nil)
+
+	server := api.NewServer(store)
+	recoder := httptest.NewRecorder()
+
+	url := fmt.Sprintf("/account/%d", account.ID)
+	request, err := http.NewRequest(http.MethodGet, url, nil)
+	require.NoError(t, err)
+
+	server.router.ServeHTTP(recoder, request)
+	require.Equal(t, http.StatusOK, recoder.Code)
+
+}
+
+func randomAccount() db.Account {
+	return db.Account{
+		ID:       utils.RandomInt(1, 1000),
+		Owner:    utils.RandomOwner(),
+		Currency: utils.RandomCurrency(),
+		Balance:  utils.RandomMoney(),
+	}
+}
